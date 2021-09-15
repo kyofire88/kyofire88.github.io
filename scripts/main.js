a2438b4e81a6bcb5975e008a3076d852477d8e50
kyofire88 <53910581+kyofire88@users.noreply.github.com>
2021-09-15 10:25:15 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
